32a3d663b424c12a327ab458a705292f48b15890
Segundo Fariña <segundofarina@me.com>
2021-10-14 19:47:45 -0300

    Final commit

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders app", () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Maze challenge/i);
-  expect(headerElement).toBeInTheDocument();
-});
